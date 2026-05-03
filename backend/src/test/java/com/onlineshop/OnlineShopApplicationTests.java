81a9de34d82abff4d1aa7532fd39457a3fd6f062
Mariusz Andrzejewski <mariusz.andrzejewski85@gmail.com>
2026-05-03 20:10:13 +0200

    test(context): run context load test on test profile with testcontainers

--- backend/src/test/java/com/onlineshop/OnlineShopApplicationTests.java
+++ backend/src/test/java/com/onlineshop/OnlineShopApplicationTests.java
@@ -1,10 +1,8 @@
 package com.onlineshop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class OnlineShopApplicationTests {
+class OnlineShopApplicationTests extends BaseIntegrationTest {
 
 	@Test
 	void contextLoads() {
